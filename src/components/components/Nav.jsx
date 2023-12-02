341cf45cbbb73bfef5674eeaf978f2116ea915d4
ruslana_hural <rusjah1@gmail.com>
2023-12-02 10:57:05 +0100

    creating welcom page

--- src/components/components/Nav.jsx
+++ src/components/components/Nav.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useAppContext } from '../../contex/AppContext'
+import { Link } from 'react-router-dom'
 
 function Nav() {
     const {authorization} = useAppContext()
   return (
     <div className="navbar px-4 bg-orange-300 min-h-[7vh]">
-        <div className="flex-1">
-            <a className="btn btn-ghost text-xl font-marhey fontt-bold text-[2em]">Todo</a>
+        <div className="flex-1 flex items-center">
+            <Link to={'/'}><a className="btn btn-ghost text-xl font-marhey fontt-bold text-[2em]">Todo</a> </Link>
+           
             <p>{authorization}</p>
         </div>
        {authorization &&  <div className="flex-none">
--- src/components/components/Wlelocom.jsx
+++ src/components/components/Wlelocom.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 function Wlelocom() {
   return (
-    <div className='flex flex-col justify-center items-center gap-4 font-dancing mask mask-pentagon w-[70vw] h-[70vh] bg-yellow-50 relative'>
-        <h1 className='font-bold text-[3em] '>Welcom to your our ToDo page</h1>
-        <p className='text-[2em]'>Please, login first</p>
-        <Link to={'/login'} className='absolute bottom-[10%]'><button className="btn btn-outline btn-success font-">Login</button></Link>
+    <div className='flex flex-col justify-center items-center gap-4 font-dancing mask mask-pentagon w-[70vw] h-[70vh] bg-yellow-50 relative pt-4 lg:pt-0'>
+        <h1 className='font-bold lg:text-[3em] '>Welcom to your our ToDo page</h1>
+        <p className='lg:text-[2em] font-marhey'>Please, login first</p>
+        <Link to={'/login'} className='lg:absolute lg:bottom-[10%]'><button className="btn btn-outline btn-success font-marhey lg:text-[2em]">Login</button></Link>
     </div>
   )
 }
